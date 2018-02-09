526816bfbe275b31141b4be6f70f3487386bcb0b
Christiaan Bakker <contact@anicator.com>
2018-02-09 09:57:32 +0100

    Updated Math.h with vec3 Normalize and Length functions.

--- Utility/Math.h
+++ Utility/Math.h
@@ -30,6 +30,33 @@ namespace Math
 		return LengthBiased;
 	}
 
+	inline float Length( glm::vec3& Vector )
+	{
+		return sqrtf( Vector[0] * Vector[0] + Vector[1] * Vector[1] + Vector[2] * Vector[2] );
+	}
+
+	inline float Normalize( glm::vec3& Vector )
+	{
+		const float LengthBiased = 1.f / ( Length( Vector ) + FLT_EPSILON );
+
+		Vector[0] *= LengthBiased;
+		Vector[1] *= LengthBiased;
+		Vector[2] *= LengthBiased;
+
+		return LengthBiased;
+	}
+
+	inline float Normalize( glm::vec3& Vector, float Length )
+	{
+		const float LengthBiased = 1.f / ( Length + FLT_EPSILON );
+
+		Vector[0] *= LengthBiased;
+		Vector[1] *= LengthBiased;
+		Vector[2] *= LengthBiased;
+
+		return LengthBiased;
+	}
+
 	struct FFrustumPlane
 	{
 
