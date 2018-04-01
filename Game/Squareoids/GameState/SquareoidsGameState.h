e05c40887ef6e5499c922578d772783856cfc483
AniCator <contact@anicator.com>
2018-04-01 15:43:30 +0200

    Created SquareoidsGameState.h file containing a game state enumerator.

---
+++ Game/Squareoids/GameState/SquareoidsGameState.h
@@ -0,0 +1,12 @@
+// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#pragma once
+
+#include <stdint.h>
+
+enum class ESquareoidGameState : int32_t
+{
+	Unknown = 0,
+	TitleScreen,
+	Game,
+	Max
+};
