37483045d077479ff57a7473c8b59e2378c74504
AniCator <contact@anicator.com>
2018-04-08 14:41:00 +0200

    Added BoundingBoxIntersection check to Math.h.

--- Engine/Utility/Math.h
+++ Engine/Utility/Math.h
@@ -57,6 +57,34 @@ namespace Math
 		return LengthBiased;
 	}
 
+	inline float Lerp( float A, float B, float Alpha )
+	{
+		return A + Alpha * ( B - A );
+	}
+
+	inline bool PointInBoundingBox( const glm::vec3& Vector, const glm::vec3& Minimum, const glm::vec3& Maximum )
+	{
+		if( Vector[0] > Minimum[0] && Vector[1] > Minimum[1] && Vector[2] > Minimum[2] &&
+			Vector[0] < Maximum[0] && Vector[1] < Maximum[1] && Vector[2] < Maximum[2] )
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	inline bool BoundingBoxIntersection( const glm::vec3& MinimumA, const glm::vec3& MaximumA, const glm::vec3& MinimumB, const glm::vec3& MaximumB )
+	{
+		if( ( MinimumA[0] < MaximumB[0] && MaximumA[0] > MinimumB[0] ) &&
+			( MinimumA[1] < MaximumB[1] && MaximumA[1] > MinimumB[1] ) &&
+			( MinimumA[2] < MaximumB[2] && MaximumA[2] > MinimumB[2] ) )
+		{
+			return true;
+		}
+
+		return false;
+	}
+
 	struct FFrustumPlane
 	{
 
