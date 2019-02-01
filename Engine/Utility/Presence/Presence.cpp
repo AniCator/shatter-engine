4a3913cd2f324c9069546f7a0b2d071a74f853ed
AniCator <contact@anicator.com>
2019-02-02 00:36:44 +0100

    Cleaned up presence code.

--- Engine/Utility/Presence/Presence.cpp
+++ Engine/Utility/Presence/Presence.cpp
@@ -1,10 +1,7 @@
 // Copyright © 2017, Christiaan Bakker, All rights reserved.
 #include "Precense.h"
 
-#include <functional>
 #include <ThirdParty/discord-rpc/win64-static/include/discord_rpc.h>
-
-#include <Engine/Utility/Timer.h>
 
 namespace Presence
 {
@@ -13,24 +10,17 @@ namespace Presence
 		DiscordEventHandlers Handlers;
 		memset( &Handlers, 0, sizeof( Handlers ) );
 
-		Handlers.ready = nullptr;
-		Handlers.errored = nullptr;
-		Handlers.disconnected = nullptr;
-		Handlers.joinGame = nullptr;
-		Handlers.spectateGame = nullptr;
-		Handlers.joinRequest = nullptr;
-
 		Discord_Initialize( ApplicationID.c_str(), &Handlers, 1, nullptr );
 	}
 
 	void Update( const char* State, const char* Details, const char* ImageKey )
 	{
-		DiscordRichPresence discordPresence;
-		memset( &discordPresence, 0, sizeof( discordPresence ) );
-		discordPresence.state = State;
-		discordPresence.details = Details;
-		discordPresence.largeImageKey = ImageKey;
-		Discord_UpdatePresence( &discordPresence );
+		DiscordRichPresence Presence;
+		memset( &Presence, 0, sizeof( Presence ) );
+		Presence.state = State;
+		Presence.details = Details;
+		Presence.largeImageKey = ImageKey;
+		Discord_UpdatePresence( &Presence );
 	}
 
 	void Shutdown()
