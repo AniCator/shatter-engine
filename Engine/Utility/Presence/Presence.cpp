332b0816b03576487b1b6428e358bf9fcb6b2441
AniCator <contact@anicator.com>
2019-02-03 13:21:14 +0100

    Don't initialize Discord RPC when the DiscordPresence preprocessor isn't defined.

--- Engine/Utility/Presence/Presence.cpp
+++ Engine/Utility/Presence/Presence.cpp
@@ -2,32 +2,41 @@
 #include "Precense.h"
 
 #include <ThirdParty/discord-rpc/win64-static/include/discord_rpc.h>
+#include <Engine/Profiling/Logging.h>
 
 namespace Presence
 {
 	void Initialize( std::string ApplicationID )
 	{
+#if defined(DiscordPresence)
 		DiscordEventHandlers Handlers;
 		memset( &Handlers, 0, sizeof( Handlers ) );
 
 		Discord_Initialize( ApplicationID.c_str(), &Handlers, 1, nullptr );
+#else
+		Log::Event( Log::Warning, "Discord presence is disabled for debug builds.\n" );
+#endif
 	}
 
 	void Update( const char* State, const char* Details, const char* ImageKey )
 	{
+#if defined(DiscordPresence)
 		DiscordRichPresence Presence;
 		memset( &Presence, 0, sizeof( Presence ) );
 		Presence.state = State;
 		Presence.details = Details;
 		Presence.largeImageKey = ImageKey;
 		Discord_UpdatePresence( &Presence );
+#endif
 	}
 
 	void Shutdown()
 	{
+#if defined(DiscordPresence)
 #ifdef DISCORD_DISABLE_IO_THREAD
 		Discord_UpdateConnection();
 #endif
 		Discord_RunCallbacks();
+#endif
 	}
 }
