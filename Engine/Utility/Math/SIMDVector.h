f56a924c1d69c819b55e9e24156db5dd66f2c7d4
AniCator <contact@anicator.com>
2019-05-08 21:15:04 +0200

    Added Vector3D class.

--- Engine/Utility/Math/SIMDVector.h
+++ Engine/Utility/Math/SIMDVector.h
@@ -40,107 +40,180 @@ namespace SIMDMath
 	}
 }*/
 
-namespace Math
+class Vector3D
 {
-	class Vector4D
+public:
+	float X, Y, Z;
+	Vector3D() = default;
+	Vector3D( float X, float Y, float Z )
 	{
-	public:
-		Vector4D()
-		{
-			StoredVector = _mm_setr_ps( 0.0f, 0.0f, 0.0f, 0.0f );
-		};
+		this->X = X;
+		this->Y = Y;
+		this->Z = Z;
+	}
 
-		Vector4D( const glm::vec3& Vector )
-		{
-			AssignVector( Vector );
-		}
+	inline float operator[]( size_t Index )
+	{
+		return ( &X )[Index];
+	}
 
-		Vector4D( const glm::vec4& Vector )
-		{
-			AssignVector( Vector );
-		}
-
-		inline void AssignVector( const glm::vec3& Vector )
-		{
-			StoredVector = _mm_setr_ps( Vector.x, Vector.y, Vector.z, 0.0f );
-		}
-
-		inline void AssignVector( const glm::vec4& Vector )
-		{
-			StoredVector = _mm_setr_ps( Vector.x, Vector.y, Vector.z, Vector.w );
-		}
-
-		inline void operator=( const glm::vec3& Vector )
-		{
-			AssignVector( Vector );
-		};
-
-		inline void operator=( const glm::vec4& Vector )
-		{
-			AssignVector( Vector );
-		};
-
-		inline glm::vec4 ToVec4() const
-		{
-			float RawVector[4];
-			_mm_storeu_ps( RawVector, StoredVector );
-			return glm::vec4( RawVector[0], RawVector[1], RawVector[2], RawVector[3] );
-		}
-
-		inline float Length()
-		{
-			return Length( StoredVector );
-		}
-
-		inline __m128 Dot( const __m128& Vector )
-		{
-			return _mm_dp_ps( StoredVector, Vector, 0x71 );
-		}
-
-		inline float Distance( const __m128& Vector )
-		{
-			return Length( Subtract( StoredVector, Vector ) );
-		}
-
-		inline __m128 operator+=( const __m128& Vector )
-		{
-			StoredVector = Add( StoredVector, Vector );
-			return StoredVector;
-		};
-
-		inline __m128 operator-=( const __m128& Vector )
-		{
-			StoredVector = Subtract( StoredVector, Vector );
-			return StoredVector;
-		};
-
-		inline __m128 operator*=( const __m128& Vector )
-		{
-			StoredVector = Multiply( StoredVector, Vector );
-			return StoredVector;
-		};
-
-		// Static functions
-		static inline __m128 Add( const __m128& A, const __m128& B )
-		{
-			return _mm_add_ps( A, B );
-		}
-
-		static inline __m128 Subtract( const __m128& A, const __m128& B )
-		{
-			return _mm_sub_ps( A, B );
-		}
-
-		static inline __m128 Multiply( const __m128& A, const __m128& B )
-		{
-			return _mm_mul_ps( A, B );
-		}
-
-		static inline float Length( const __m128& Vector )
-		{
-			return _mm_cvtss_f32( _mm_sqrt_ss( _mm_dp_ps( Vector, Vector, 0x71 ) ) );
-		}
-	private:
-		__m128 StoredVector;
+	inline Vector3D operator+=( const Vector3D& Vector )
+	{
+		X += Vector.X;
+		Y += Vector.Y;
+		Z += Vector.Z;
+		return *this;
 	};
-}
+
+	inline Vector3D operator-=( const Vector3D& Vector )
+	{
+		X -= Vector.X;
+		Y -= Vector.Y;
+		Z -= Vector.Z;
+		return *this;
+	};
+
+	inline Vector3D operator*=( const Vector3D& Vector )
+	{
+		X *= Vector.X;
+		Y *= Vector.Y;
+		Z *= Vector.Z;
+		return *this;
+	};
+
+	inline Vector3D operator+=( const float& Scalar )
+	{
+		X += Scalar;
+		Y += Scalar;
+		Z += Scalar;
+		return *this;
+	};
+
+	inline Vector3D operator-=( const float& Scalar )
+	{
+		X -= Scalar;
+		Y -= Scalar;
+		Z -= Scalar;
+		return *this;
+	};
+
+	inline Vector3D operator*=( const float& Scalar )
+	{
+		X *= Scalar;
+		Y *= Scalar;
+		Z *= Scalar;
+		return *this;
+	};
+
+	inline float Dot( const Vector3D& Vector ) const
+	{
+		return X * Vector.X + Y * Vector.Y + Z * Vector.Z;
+	}
+
+	inline Vector3D Cross( const Vector3D& Vector ) const
+	{
+		return Vector3D(	Y * Vector.Z - Vector.Y * Z,
+							Z * Vector.X - Vector.Z * X,
+							X * Vector.Y - Vector.X * Y	);
+	}
+};
+
+class Vector4D
+{
+public:
+	Vector4D() = default;
+
+	Vector4D( const glm::vec3& Vector )
+	{
+		AssignVector( Vector );
+	}
+
+	Vector4D( const glm::vec4& Vector )
+	{
+		AssignVector( Vector );
+	}
+
+	inline void AssignVector( const glm::vec3& Vector )
+	{
+		StoredVector = _mm_setr_ps( Vector.x, Vector.y, Vector.z, 0.0f );
+	}
+
+	inline void AssignVector( const glm::vec4& Vector )
+	{
+		StoredVector = _mm_setr_ps( Vector.x, Vector.y, Vector.z, Vector.w );
+	}
+
+	inline void operator=( const glm::vec3& Vector )
+	{
+		AssignVector( Vector );
+	};
+
+	inline void operator=( const glm::vec4& Vector )
+	{
+		AssignVector( Vector );
+	};
+
+	inline glm::vec4 ToVec4() const
+	{
+		float RawVector[4];
+		_mm_storeu_ps( RawVector, StoredVector );
+		return glm::vec4( RawVector[0], RawVector[1], RawVector[2], RawVector[3] );
+	}
+
+	inline float Length()
+	{
+		return Length( StoredVector );
+	}
+
+	inline __m128 Dot( const __m128& Vector )
+	{
+		return _mm_dp_ps( StoredVector, Vector, 0x71 );
+	}
+
+	inline float Distance( const __m128& Vector )
+	{
+		return Length( Subtract( StoredVector, Vector ) );
+	}
+
+	inline __m128 operator+=( const __m128& Vector )
+	{
+		StoredVector = Add( StoredVector, Vector );
+		return StoredVector;
+	};
+
+	inline __m128 operator-=( const __m128& Vector )
+	{
+		StoredVector = Subtract( StoredVector, Vector );
+		return StoredVector;
+	};
+
+	inline __m128 operator*=( const __m128& Vector )
+	{
+		StoredVector = Multiply( StoredVector, Vector );
+		return StoredVector;
+	};
+
+	// Static functions
+	static inline __m128 Add( const __m128& A, const __m128& B )
+	{
+		return _mm_add_ps( A, B );
+	}
+
+	static inline __m128 Subtract( const __m128& A, const __m128& B )
+	{
+		return _mm_sub_ps( A, B );
+	}
+
+	static inline __m128 Multiply( const __m128& A, const __m128& B )
+	{
+		return _mm_mul_ps( A, B );
+	}
+
+	static inline float Length( const __m128& Vector )
+	{
+		return _mm_cvtss_f32( _mm_sqrt_ss( _mm_dp_ps( Vector, Vector, 0x71 ) ) );
+	}
+
+private:
+	__m128 StoredVector;
+};
