59fd6d4f609647804f080f339ae1fcff9f497961
AniCator <contact@anicator.com>
2019-05-26 23:17:00 +0200

    Added support for getting the width and height of a texture. Added a default constructor.

--- Engine/Display/Rendering/Texture.cpp
+++ Engine/Display/Rendering/Texture.cpp
@@ -8,41 +8,17 @@
 #include <Engine/Utility/Data.h>
 #include <Engine/Utility/File.h>
 
-static const GLenum EnumToSlot[ETextureSlot::Maximum]
+CTexture::CTexture()
 {
-	GL_TEXTURE0,
-	GL_TEXTURE1,
-	GL_TEXTURE2,
-	GL_TEXTURE3,
-	GL_TEXTURE4,
-	GL_TEXTURE5,
-	GL_TEXTURE6,
-	GL_TEXTURE7,
-	GL_TEXTURE8,
-	GL_TEXTURE9,
-	GL_TEXTURE10,
-	GL_TEXTURE11,
-	GL_TEXTURE12,
-	GL_TEXTURE13,
-	GL_TEXTURE14,
-	GL_TEXTURE15,
-	GL_TEXTURE16,
-	GL_TEXTURE17,
-	GL_TEXTURE18,
-	GL_TEXTURE19,
-	GL_TEXTURE20,
-	GL_TEXTURE21,
-	GL_TEXTURE22,
-	GL_TEXTURE23,
-	GL_TEXTURE24,
-	GL_TEXTURE25,
-	GL_TEXTURE26,
-	GL_TEXTURE27,
-	GL_TEXTURE28,
-	GL_TEXTURE29,
-	GL_TEXTURE30,
-	GL_TEXTURE31
-};
+	Location = "";
+
+	Handle = 0;
+	Width = 0;
+	Height = 0;
+	Channels = 0;
+
+	ImageData = nullptr;
+}
 
 CTexture::CTexture( const char* FileLocation )
 {
@@ -150,4 +126,14 @@ void CTexture::Bind( ETextureSlot Slot )
 		glActiveTexture( EnumToSlot[Index] );
 		glBindTexture( GL_TEXTURE_2D, Handle );
 	}
+}
+
+const int CTexture::GetWidth() const
+{
+	return Width;
+}
+
+const int CTexture::GetHeight() const
+{
+	return Height;
 }
--- Engine/Display/Rendering/Texture.h
+++ Engine/Display/Rendering/Texture.h
@@ -84,17 +84,56 @@ static const char* TextureSlotName[TextureSlots] = {
 	"Slot31"
 };
 
+static const GLenum EnumToSlot[ETextureSlot::Maximum]
+{
+	GL_TEXTURE0,
+	GL_TEXTURE1,
+	GL_TEXTURE2,
+	GL_TEXTURE3,
+	GL_TEXTURE4,
+	GL_TEXTURE5,
+	GL_TEXTURE6,
+	GL_TEXTURE7,
+	GL_TEXTURE8,
+	GL_TEXTURE9,
+	GL_TEXTURE10,
+	GL_TEXTURE11,
+	GL_TEXTURE12,
+	GL_TEXTURE13,
+	GL_TEXTURE14,
+	GL_TEXTURE15,
+	GL_TEXTURE16,
+	GL_TEXTURE17,
+	GL_TEXTURE18,
+	GL_TEXTURE19,
+	GL_TEXTURE20,
+	GL_TEXTURE21,
+	GL_TEXTURE22,
+	GL_TEXTURE23,
+	GL_TEXTURE24,
+	GL_TEXTURE25,
+	GL_TEXTURE26,
+	GL_TEXTURE27,
+	GL_TEXTURE28,
+	GL_TEXTURE29,
+	GL_TEXTURE30,
+	GL_TEXTURE31
+};
+
 class CTexture
 {
 public:
+	CTexture();
 	CTexture( const char* FileLocation );
-	CTexture( unsigned char* Pixels, int TextureWidth, int TextureHeight );
 	~CTexture();
 
 	bool Load();
 	void Bind( ETextureSlot Slot );
 
-private:
+	const int GetWidth() const;
+	const int GetHeight() const;
+
+protected:
 	GLuint Handle;
 	std::string Location;
 
