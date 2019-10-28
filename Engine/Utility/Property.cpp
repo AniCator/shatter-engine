6810609d03ff338fb6d7f05965bce62f491f539d
AniCator <contact@anicator.com>
2019-10-28 14:11:08 +0100

    Added Property struct that will potentially be used for data storage.

---
+++ Engine/Utility/Property.cpp
@@ -0,0 +1,210 @@
+// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#include "Property.h"
+
+#include <Engine/Utility/DataString.h>
+
+Property::Property( const std::string& Value )
+{
+	String = Value;
+}
+
+Property::Property( const uint32_t& Value )
+{
+	Unsigned32 = Value;
+}
+
+Property::Property( const uint16_t& Value )
+{
+	Unsigned16 = Value;
+}
+
+Property::Property( const uint8_t& Value )
+{
+	Unsigned8 = Value;
+}
+
+Property::Property( const int32_t& Value )
+{
+	Signed32 = Value;
+}
+
+Property::Property( const int16_t& Value )
+{
+	Signed16 = Value;
+}
+
+Property::Property( const int8_t& Value )
+{
+	Signed8 = Value;
+}
+
+Property::Property( const bool& Value )
+{
+	Boolean = Value;
+}
+
+const static std::string Empty = "";
+const std::string& Property::GetString() const
+{
+	if( Type == PropertyType::String )
+	{
+		return String;
+	}
+
+	return Empty;
+}
+
+const static uint32_t EmptyU32 = 0;
+const uint32_t& Property::GetU32() const
+{
+	if( Type == PropertyType::U32 )
+	{
+		return Unsigned32;
+	}
+
+	return EmptyU32;
+}
+
+const static uint16_t EmptyU16 = 0;
+const uint16_t& Property::GetU16() const
+{
+	if( Type == PropertyType::U16 )
+	{
+		return Unsigned16;
+	}
+
+	return EmptyU16;
+}
+
+const static uint8_t EmptyU8 = 0;
+const uint8_t& Property::GetU8() const
+{
+	if( Type == PropertyType::U8 )
+	{
+		return Unsigned8;
+	}
+
+	return EmptyU8;
+}
+
+const static int32_t EmptyI32 = 0;
+const int32_t& Property::GetI32() const
+{
+	if( Type == PropertyType::I32 )
+	{
+		return Signed32;
+	}
+
+	return EmptyI32;
+}
+
+const static int16_t EmptyI16 = 0;
+const int16_t& Property::GetI16() const
+{
+	if( Type == PropertyType::I16 )
+	{
+		return Signed16;
+	}
+
+	return EmptyI16;
+}
+
+const static int8_t EmptyI8 = 0;
+const int8_t& Property::GetI8() const
+{
+	if( Type == PropertyType::I8 )
+	{
+		return Signed8;
+	}
+
+	return EmptyI8;
+}
+
+const static bool EmptyBoolean = false;
+const bool& Property::GetBoolean() const
+{
+	if( Type == PropertyType::Boolean )
+	{
+		return Boolean;
+	}
+
+	return EmptyBoolean;
+}
+
+CData& operator<<( CData& Data, Property& Value )
+{
+	Data << Value.Type;
+	if( Value.Type == PropertyType::String )
+	{
+		FDataString::Encode( Data, Value.String );
+	}
+	else if( Value.Type == PropertyType::U32 )
+	{
+		Data << Value.Unsigned32;
+	}
+	else if( Value.Type == PropertyType::U16 )
+	{
+		Data << Value.Unsigned16;
+	}
+	else if( Value.Type == PropertyType::U8 )
+	{
+		Data << Value.Unsigned8;
+	}
+	else if( Value.Type == PropertyType::I32 )
+	{
+		Data << Value.Signed32;
+	}
+	else if( Value.Type == PropertyType::I16 )
+	{
+		Data << Value.Signed16;
+	}
+	else if( Value.Type == PropertyType::I8 )
+	{
+		Data << Value.Signed8;
+	}
+	else if( Value.Type == PropertyType::Boolean )
+	{
+		Data << Value.Boolean;
+	}
+
+	return Data;
+}
+
+CData& operator>>( CData& Data, Property& Value )
+{
+	Data >> Value.Type;
+	if( Value.Type == PropertyType::String )
+	{
+		FDataString::Decode( Data, Value.String );
+	}
+	else if( Value.Type == PropertyType::U32 )
+	{
+		Data >> Value.Unsigned32;
+	}
+	else if( Value.Type == PropertyType::U16 )
+	{
+		Data >> Value.Unsigned16;
+	}
+	else if( Value.Type == PropertyType::U8 )
+	{
+		Data >> Value.Unsigned8;
+	}
+	else if( Value.Type == PropertyType::I32 )
+	{
+		Data >> Value.Signed32;
+	}
+	else if( Value.Type == PropertyType::I16 )
+	{
+		Data >> Value.Signed16;
+	}
+	else if( Value.Type == PropertyType::I8 )
+	{
+		Data >> Value.Signed8;
+	}
+	else if( Value.Type == PropertyType::Boolean )
+	{
+		Data >> Value.Boolean;
+	}
+
+	return Data;
+}
---
+++ Engine/Utility/Property.h
@@ -0,0 +1,77 @@
+// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#pragma once
+
+#include <string>
+#include <Engine/Utility/Data.h>
+#include <Engine/Utility/Structures/Name.h>
+
+enum class PropertyType : uint8_t
+{
+	String,
+	U32,
+	U16,
+	U8,
+
+	I32,
+	I16,
+	I8,
+
+	Boolean
+};
+
+struct Property
+{
+public:
+	Property( const std::string& Value );
+
+	Property( const uint32_t& Value );
+	Property( const uint16_t& Value );
+	Property( const uint8_t& Value );
+
+	Property( const int32_t& Value );
+	Property( const int16_t& Value );
+	Property( const int8_t& Value );
+
+	Property( const bool& Value );
+
+	~Property() {};
+
+	const std::string& GetString() const;
+
+	const uint32_t& GetU32() const;
+	const uint16_t& GetU16() const;
+	const uint8_t& GetU8() const;
+
+	const int32_t& GetI32() const;
+	const int16_t& GetI16() const;
+	const int8_t& GetI8() const;
+
+	const bool& GetBoolean() const;
+
+	friend CData& operator<<( CData& Data, Property& Value );
+	friend CData& operator>>( CData& Data, Property& Value );
+
+private:
+	PropertyType Type;
+
+	union
+	{
+		std::string String;
+
+		uint32_t Unsigned32;
+		uint16_t Unsigned16;
+		uint8_t Unsigned8;
+
+		int32_t Signed32;
+		int16_t Signed16;
+		int8_t Signed8;
+
+		bool Boolean;
+	};
+};
+
+struct PropertyPair
+{
+	FName Key;
+	Property Value;
+};
