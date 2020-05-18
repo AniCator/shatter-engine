3edd3442bf7c7d3dacc32029131dc4afd0212585
AniCator <contact@anicator.com>
2020-05-18 15:58:19 -0600

    Moved Worker and Task structs into the engine codebase.

---
+++ Engine/Utility/Thread.h
@@ -0,0 +1,95 @@
+// Copyright © 2017, Christiaan Bakker, All rights reserved.
+
+#pragma once
+
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+struct Task
+{
+	Task()
+	{
+		// Default constructor is left empty.
+	}
+
+	Task(std::function<void()> ToExecute)
+	{
+		Function = ToExecute;
+	}
+
+	void Execute()
+	{
+		Function();
+	}
+
+private:
+	std::function<void()> Function;
+};
+
+struct Worker
+{
+	Worker()
+	{
+		Ready = false;
+		Running = false;
+		Alive = true;
+		Thread = std::thread(&Worker::Work, this);
+	}
+
+	~Worker()
+	{
+		Alive = false;
+		Notify.notify_one();
+		Thread.join();
+	}
+
+	bool Completed() const
+	{
+		return !Ready;
+	}
+
+	bool IsRunning() const
+	{
+		return Running;
+	}
+
+	void Start(Task& ToExecute)
+	{
+		std::unique_lock<std::mutex> Lock(Mutex);
+
+		Task = ToExecute;
+		Ready = true;
+
+		Notify.notify_one();
+	}
+
+private:
+	void Work()
+	{
+		while (Alive)
+		{
+			std::unique_lock<std::mutex> Lock(Mutex);
+			Notify.wait(Lock);
+
+			Running = true;
+
+			Task.Execute();
+
+			Running = false;
+			Ready = false;
+		}
+	}
+
+	std::thread Thread;
+	Task Task;
+
+	std::mutex Mutex;
+	std::condition_variable Notify;
+
+	std::atomic<bool> Ready;
+	std::atomic<bool> Running;
+	std::atomic<bool> Alive;
+};
