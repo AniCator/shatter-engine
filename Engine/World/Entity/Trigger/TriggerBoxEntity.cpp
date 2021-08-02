a8c20aa9d5dc3e1fb85c5d68f601863d596aeb35
AniCator <contact@anicator.com>
2021-08-01 23:14:39 -0600

    Added early implementation of new trigger_box entity.

---
+++ Engine/World/Entity/Trigger/TriggerBoxEntity.cpp
@@ -0,0 +1,89 @@
+﻿// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#include "TriggerBoxEntity.h"
+
+#include <Engine/World/World.h>
+
+static CEntityFactory<CTriggerBoxEntity> Factory( "trigger_box" );
+
+void CTriggerBoxEntity::Construct()
+{
+	if( !Volume )
+	{
+		Volume = new CTriggerBody<Interactable*>( this );
+		Volume->Construct();
+
+		const auto Size = Vector3D( 1.0f );
+		Volume->SetBounds( FBounds( -Size, Size ) );
+	}
+}
+
+void CTriggerBoxEntity::Tick()
+{
+	auto* World = GetWorld();
+	if( World )
+	{
+		const auto& CameraSetup = World->GetActiveCameraSetup();
+		const auto Delta = CameraSetup.CameraPosition - Transform.GetPosition();
+
+		float Length = Delta.Length();
+		if( !Volume->Entities.empty() )
+		{
+			if( !Latched && ( Frequency < 0 || Count < Frequency ) )
+			{
+				Send( "OnTrigger" );
+				Latched = true;
+
+				Count++;
+			}
+		}
+		else
+		{
+			if( Latched )
+			{
+				Latched = false;
+			}
+		}
+	}
+}
+
+void CTriggerBoxEntity::Destroy()
+{
+	if( Volume )
+	{
+		Volume->Destroy();
+		delete Volume;
+	}
+}
+
+void CTriggerBoxEntity::Load( const JSON::Vector& Objects )
+{
+	CPointEntity::Load( Objects );
+
+	for( const auto* Property : Objects )
+	{
+		if( Property->Key == "frequency" )
+		{
+			const double PropertyFrequency = ParseDouble( Property->Value.c_str() );
+			if( PropertyFrequency > 0.0f )
+			{
+				Frequency = static_cast<int32_t>( PropertyFrequency );
+			}
+		}
+	}
+}
+
+void CTriggerBoxEntity::Export( CData& Data )
+{
+	CPointEntity::Export( Data );
+	Data << Frequency;
+	Data << Latched;
+	Data << Count;
+}
+
+void CTriggerBoxEntity::Import( CData& Data )
+{
+	CPointEntity::Import( Data );
+	Data >> Frequency;
+	Data >> Latched;
+	Data >> Count;
+}
---
+++ Engine/World/Entity/Trigger/TriggerBoxEntity.h
@@ -0,0 +1,29 @@
+﻿// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#pragma once
+
+#include <Engine/Physics/PhysicsComponent.h>
+#include <Engine/World/Entity/PointEntity/PointEntity.h>
+
+class CTriggerBoxEntity : public CPointEntity
+{
+public:
+	CTriggerBoxEntity() = default;
+
+	virtual void Construct() override;
+	virtual void Tick() override;
+	virtual void Destroy() override;
+	virtual void Load( const JSON::Vector& Objects ) override;
+
+	virtual void Export( CData& Data ) override;
+	virtual void Import( CData& Data ) override;
+
+private:
+	bool Latched = false;
+	int32_t Frequency = -1;
+	int32_t Count = 0;
+
+	const FBounds DefaultBounds = FBounds( Vector3D( -1.0f ), Vector3D( 1.0f ) );
+	FBounds Bounds = DefaultBounds;
+
+	CTriggerBody<Interactable*>* Volume = nullptr;
+};
