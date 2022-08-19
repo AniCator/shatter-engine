adf51b5358da246b0e2c97fd7b43317d2a8f8696
AniCator <contact@anicator.com>
2022-08-19 14:34:11 +0200

    Strict bitflag wrapper template.

---
+++ Engine/Utility/Flag.h
@@ -0,0 +1,31 @@
+// Copyright © 2017, Christiaan Bakker, All rights reserved.
+#pragma once
+
+#include <type_traits>
+
+template<typename T>
+struct Flag
+{
+	using Type = std::underlying_type_t<T>;
+	Type State;
+
+	Flag( T State )
+	{
+		this->State = static_cast<Type>( State );
+	}
+
+	void Set( T Flags )
+	{
+		State |= static_cast<Type>( Flags );
+	}
+
+	void Remove( T Flags )
+	{
+		State &= ~static_cast<Type>( Flags );
+	}
+
+	bool Has( T Flags ) const
+	{
+		return ( State & static_cast<Type>( Flags ) ) != 0;
+	}
+};
